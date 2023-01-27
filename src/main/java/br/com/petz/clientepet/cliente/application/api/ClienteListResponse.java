1b067df42171f1c2f0357c8da9e2ccb567323949
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-01-27 06:52:34 -0300

    implementa getTodos

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -2,6 +2,7 @@ package br.com.petz.clientepet.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
 import lombok.Value;
@@ -9,12 +10,23 @@ import lombok.Value;
 public class ClienteListResponse {
 
 	private UUID idCliente;
-	private String nome;
+	private String nomeCompleto;
 	private String cpf;
 	private String email;
+	private String celular;
 	
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+		return clientes.stream().map(ClienteListResponse::new).collect(Collectors.toList());
 	}
+
+	public ClienteListResponse(Cliente cliente) {
+		super();
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.cpf = cliente.getCpf();
+		this.email = cliente.getEmail();
+		this.celular = cliente.getCelular();
+	}
+
 
 }
