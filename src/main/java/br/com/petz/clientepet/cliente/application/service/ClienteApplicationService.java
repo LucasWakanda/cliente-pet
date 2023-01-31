ef94c07be7edee7a150184ac23a0b6e35c984f3d
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-01-31 14:02:51 -0300

    ClienteDetalhado implementado com sucesso

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -42,8 +42,9 @@ return ClienteListResponse.converte(clientes);
 	@Override
 	public ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente) {
 		log.info("[inicia] ClienteApplicationService -  buscaClienteAtravesId ");
+		Cliente cliente = clienteRepository. buscaClienteAtravesId(idCliente);
 		log.info("[finaliza] ClienteApplicationService -  buscaClienteAtravesId ");
-		return null;
+		return new ClienteDetalhadoResponse(cliente);
 	}
 
 }
